1a599b13a8c292d611b4b1d6dad6661b7c7ab736
Rutul <rutul.patel@ibm.com>
2020-04-20 13:56:15 -0300

    Da2 started

---
+++ day2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	helpers "../helpers"
+)
+
+func main() {
+	puzzleInputFile := helpers.GetFile("./input.txt")
+	puzzleInputs := []int{}
+
+	fmt.Println(puzzleInputs)
+}
